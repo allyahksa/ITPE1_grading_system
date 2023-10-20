9a447f6319b030bcd1a148768a9c6406dc0b8871
allyahksa <aadrineda21@gmail.com>
2023-10-20 15:15:51 +0800

    second commit

--- grading_system_aa/src/app/app-routing.module.ts
+++ grading_system_aa/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { StudentsComponent } from './layout/students/students.component';
+import { TeachersComponent } from './layout/teachers/teachers.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'login', pathMatch:'full'},
   { path: 'dashboard', redirectTo:'dashboard', pathMatch:'full'},
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'registration', component: RegistrationComponent }
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'teachers', component: TeachersComponent }
 ];
 
 
